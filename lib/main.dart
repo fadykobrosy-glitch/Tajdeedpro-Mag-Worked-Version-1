5c974fc3e5401088766784458c06e608c71ef5a8
PHADI <fadykobrosy@gmail.com>
2026-02-24 00:41:48 +0300

    Fix pull-to-refresh functionality by adding AlwaysScrollableScrollPhysics

--- lib/main.dart
+++ lib/main.dart
@@ -297,6 +297,7 @@ class _WebViewScreenState extends State<WebViewScreen> {
                 strokeWidth: 3,
                 child: WebViewWidget(
                   controller: _controller,
+                  physics: const AlwaysScrollableScrollPhysics(),
                 ),
               ),
             ),
